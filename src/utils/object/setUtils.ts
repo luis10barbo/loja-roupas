12df982ebcf5180255ca66203aceb3f5092d0dfb
luis10barbo <luis10barbo2020@gmail.com>
2023-04-11 13:23:02 -0300

    added utilities to help with set modifications

---
+++ src/utils/object/setUtils.ts
@@ -0,0 +1,25 @@
+export function removeFromSet<T>(set: Set<T>, value: T) {
+  const newSet = new Set(set);
+  newSet.delete(value);
+  return newSet;
+}
+
+export function addToSet<T>(set: Set<T>, value: T | T[]) {
+  if (Array.isArray(value)) {
+    const newSet = new Set(set);
+    value.forEach((arrayValue) => newSet.add(arrayValue));
+    return newSet;
+  }
+  return new Set(set.add(value));
+}
+
+export function removeEnumFromSet<T, E extends object>(
+  set: Set<T | E[keyof E]>,
+  enumObject: E
+) {
+  const newSet = new Set(set);
+  Object.values(enumObject).map((value) => {
+    newSet.delete(value);
+  });
+  return newSet;
+}
